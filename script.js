f84d37721ff2f47ec6ec1622f06ce7d479fd253d
ianckruger <ian.kruger.2@gmail.com>
2025-11-03 14:57:35 -0500

    implementing basic sendMessage function

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const chatForm = document.getElementById("chatForm");
 const userInput = document.getElementById("userInput");
 const chatWindow = document.getElementById("chatWindow");
 
+const workerURL = "https://loreal-chatbot.ian-kruger-2.workers.dev/"
+
 // Set initial message
 chatWindow.textContent = "👋 Hello! How can I help you today?";
 
@@ -16,3 +18,26 @@ chatForm.addEventListener("submit", (e) => {
   // Show message
   chatWindow.innerHTML = "Connect to the OpenAI API for a response!";
 });
+
+
+async function sendMessage(userMessage) {
+  const response = await fetch(workerURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      messages: [
+        {
+          role: "system",
+          content: "You are a L'Oreal virtual beauty assistant. Provide accurate, safe, friendly, and professional beauty and skincare advice, focusing on L'Oreal products where relevant (AKA, recommend them L'Oreal products if possible).",
+        },
+        { role: "user", content: userMessage },
+      ],
+    }),
+  });
+
+  const data = await response.json();
+  const reply = data.choices?.[0].message?.content ?? "Sorry, I couldn't get a response. Please try again.";
+  return reply;
+}
